1c6f6ce6cdde567e3daab92d5c09ea55dce521b6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-12 00:46:00 -0700

    Initial Commit

---
+++ CenterImageScroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CenterImageScroller
+//
+//  Created by Ronald Hernandez on 10/12/15.
+//  Copyright © 2015 Hardcoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CenterImageScroller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CenterImageScroller
+//
+//  Created by Ronald Hernandez on 10/12/15.
+//  Copyright © 2015 Hardcoder. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
