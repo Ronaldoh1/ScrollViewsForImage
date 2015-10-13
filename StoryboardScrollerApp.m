b7b80902d17cfdd0c7da9b686d55dd8a4e004d71
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-12 23:09:35 -0700

    able center image

---
+++ StoryboardScrollerApp.m
@@ -0,0 +1,21 @@
+//
+//  StoryboardScrollerApp.m
+//  CenterImageScroller
+//
+//  Created by Ronald Hernandez on 10/12/15.
+//  Copyright © 2015 Hardcoder. All rights reserved.
+//
+
+#import "StoryboardScrollerApp.h"
+
+@implementation StoryboardScrollerApp
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
